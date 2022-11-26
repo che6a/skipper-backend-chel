a268202a27b31e0f712adabec8e897dec664b5db
Egor <ejs.defolt@gmail.com>
2022-11-26 20:25:42 +0500

    fix: disable security

--- src/main/java/com/tinkoff/skipper/config/SecurityConfig.java
+++ src/main/java/com/tinkoff/skipper/config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
